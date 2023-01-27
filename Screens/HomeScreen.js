a7cb59472e2b1d23038f6397f9f7f73c529c5831
Yash Jain <yash180501@gmail.com>
2023-01-27 13:46:35 +0530

    Update the Style for the friend's location and add the user current location button

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -3,82 +3,139 @@ import React, { useEffect, useState } from "react";
 import MapView, { Marker, MarkerAnimated } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
-import data from'../Data/FriendsData.json';
-import { ScrollView, Text } from "react-native";
+import data from '../Data/FriendsData.json';
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { responsiveScreenHeight, responsiveScreenWidth, useResponsiveScreenHeight } from "react-native-responsive-dimensions";
 
-const HomeScreen = () =>{
-    const [position, setPosition] = useState(data.result[0].location);
-      const [position2, setPosition2] = useState({
-        latitude: 100,
-        longitude: 10,
-        latitudeDelta: 0.001,
-        longitudeDelta: 0.001,
+import {getDistance, getPreciseDistance} from 'geolib';
+
+const HomeScreen = () => {
+  const [position, setPosition] = useState(data.result[0].location);
+  const [currentlocation, setCurrentlocation] = useState(true);
+
+  var dis = getDistance(
+    {latitude: 20.0504188, longitude: 64.4139099},
+    {latitude: 51.528308, longitude: -0.3817765},
+  );
+
+  // alert(
+  //   `Distance\n\n${dis} Meter\nOR\n${dis / 1000} KM`
+  // );
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition((pos) => {
+      const crd = pos.coords;
+      setPosition({
+        latitude: crd.latitude,
+        longitude: crd.longitude,
+        latitudeDelta: 0.0421,
+        longitudeDelta: 0.0421,
       });
-
-    useEffect(() => {
-        Geolocation.getCurrentPosition((pos) => {
-          const crd = pos.coords;
-          setPosition({
-            latitude: crd.latitude,
-            longitude: crd.longitude,
-            latitudeDelta: 0.0421,
-            longitudeDelta: 0.0421,
-          });
-          setPosition2({
-            latitude: crd.latitude+0.1,
-            longitude: crd.longitude+0.1,
-            latitudeDelta: 0.0421,
-            longitudeDelta: 0.0421,
-          });
-        })
-      }, []);
-    return(
-        <MapView 
-        style={{
-            flex: 1
-        }} 
-            initialRegion={position}
-            showsUserLocation={true}
-            // showsMyLocationButton={false}
-            followsUserLocation={true}
-            showsCompass={true}
-            scrollEnabled={true}
-            zoomEnabled={true}
-            pitchEnabled={true}
-            rotateEnabled={true} 
-            loadingEnabled={true}
-            
-            >
-            <Marker
-                anchor={{ x: 100 }}
-                image={{ uri: data.result[0].image }}
+    })
+  }, []);
+  return (
+    <MapView
+      style={{
+        flex: 1
+      }}
+      initialRegion={position}
+      showsUserLocation={currentlocation}
+      showsMyLocationButton={false}
+      followsUserLocation={true}
+      showsCompass={true}
+      scrollEnabled={true}
+      zoomEnabled={true}
+      pitchEnabled={true}
+      rotateEnabled={true}
+      loadingEnabled={true}
+      onTouchStart={()=>{
+        setCurrentlocation(false)
+      }}
+    >
+      <TouchableOpacity
+      onPress={()=>{
+        setCurrentlocation(true)
+      }}
+      >
+      <Image source={require('../assets/currentlocation.png')}
+      style={{
+        width:responsiveScreenHeight(6),
+        height:responsiveScreenHeight(6.2),
+        alignSelf:'flex-end',
+        marginTop:60,
+        marginRight:30
+      }}
+      />
+      </TouchableOpacity>
+      <Marker
+        title='Yor are here'
+        description='This is a description'
+        coordinate={position}
+        >
+        <View>
+          <Image source={require('../assets/location.png')}
+            style={{
+              width: responsiveScreenWidth(16),
+              height: responsiveScreenWidth(18.5),
+              position: 'absolute',
+              left: -responsiveScreenWidth(8),
+              bottom: 0,
+            }}
+          />
+          <Image source={{ uri: data.result[0].image }}
+            style={{
+              position: 'absolute',
+              width: responsiveScreenWidth(12),
+              height: responsiveScreenWidth(12),
+              borderRadius: 100,
+              left: -responsiveScreenWidth(6.2),
+              top: -responsiveScreenWidth(16.5),
+              borderWidth:2,
+              borderColor:'white',
+            }}
+          />
+        </View>
+      </Marker>
+      {[...Array(data.result.length)].map((value, index) => (
+        <>
+          <Marker
+            title={data.result[index].name}
+            description='This is a description'
+            coordinate={{
+              latitude: data.result[index].location.latitude,
+              longitude: data.result[index].location.longitude,
+              latitudeDelta: data.result[index].location.latitudeDelta,
+              longitudeDelta: data.result[index].location.longitudeDelta,
+            }}
+          >
+            <View>
+              <Image source={require('../assets/location.png')}
                 style={{
-                    borderRadius: 20,
-                    borderWidth:1,
+                  width: responsiveScreenWidth(15),
+                  height: responsiveScreenWidth(18.5),
+                  position: 'absolute',
+                  left: 0,
+                  bottom: 0,
                 }}
-
-                title='Yor are here'
-                description='This is a description'
-                coordinate={position}
-            />
-            {[...Array(data.result.length)].map((value,index)=>(
-              <>
-                <Marker
-                  // anchor={{ x: 100 }}
-                  image={{ uri: data.result[index].image }}
-                  title={data.result[index].name}
-                  description='This is a description'
-                  coordinate={{
-                    latitude: data.result[index].location.latitude,
-                    longitude: data.result[index].location.longitude,
-                    latitudeDelta: data.result[index].location.latitudeDelta,
-                    longitudeDelta: data.result[index].location.longitudeDelta,
-                  }}
-                />
-              </>
-              ))}
-            </MapView>
-    );
+              />
+              <Image source={{ uri: data.result[index].image }}
+                style={{
+                  position: 'absolute',
+                  width: responsiveScreenWidth(12),
+                  height: responsiveScreenWidth(12),
+                  borderRadius: 100,
+                  left: responsiveScreenWidth(1.5),
+                  top: -responsiveScreenWidth(16.5),
+                  borderWidth:2,
+                  borderColor:'white',
+                }}
+              />
+            </View>
+          </Marker>
+        </>
+      ))}
+    </MapView>
+  );
 };
 
 export default HomeScreen;
